ac5a4f4d1696e438d50be195f8af58d3f8354855
alireza <alirezaakbarzadeh12@gmail.com>
2025-03-21 15:39:34 +0330

    create expense and income model

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from web.models import Expense, Income
 
-# Register your models here.
+
+@admin.register(Expense)
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ('title' , 'user' , 'amount' , 'created' , 'updated')
+
+
+@admin.register(Income)
+class IncomeAdmin(admin.ModelAdmin):
+    list_display = ('title' , 'user' , 'amount' , 'created' , 'updated')
--- web/models.py
+++ web/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Expense(models.Model):
+    title = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.BigIntegerField(default=0)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.title
+
+
+class Income(models.Model):
+    title = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.BigIntegerField(default=0)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
