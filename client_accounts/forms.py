cbe3405f5f03517d0ecda83807cf7585db8fe804
alireza <alirezaakbarzadeh12@gmail.com>
2025-06-28 14:08:04 +0330

    fix(register): prevent re-registration with inactive email and show proper message

--- client_accounts/forms.py
+++ client_accounts/forms.py
@@ -17,6 +17,22 @@ class RegisterForm(forms.ModelForm):
             raise forms.ValidationError("Passwords do not match.")
         return cleaned_data
 
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        user_qs = User.objects.filter(email=email)
+        if user_qs.exists():
+            user = user_qs.first()
+            if not user.is_active :
+                raise forms.ValidationError(
+                    "این ایمیل قبلاً ثبت‌نام کرده ولی هنوز فعال نشده. "
+                    "لطفاً ایمیل خود را بررسی کنید یا درخواست ارسال مجدد لینک تأیید کنید."
+                )
+            else:
+                raise forms.ValidationError("این ایمیل قبلاً ثبت‌نام کرده است.")
+        return email
+
+
     def save(self, commit=True):
         user = super().save(commit=False)
         user.set_password(self.cleaned_data['password1'])
